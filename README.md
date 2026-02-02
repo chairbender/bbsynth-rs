5bfd8ba90f446c8f4f17f9a8992bfecbfb0acbbc
chairbender <kwhipke1@gmail.com>
2026-02-01 21:22:26 -0800

    wip getting basic gain example to work with iced 0.14

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # bbsynth-rs
 Fun with virtual analog - now in Rust + Faust
+
+todo:
+- probably need to fork nih_plug_iced and get rid of the POINT stuff that's not cross platform.
+  better option seems to be https://github.com/robbert-vdh/nih-plug/pull/170 and https://github.com/BillyDM/iced_baseview/pull/43
+
+# Attribution
+
+# License
+
+All code licensed under GPLv3
---
+++ src/editor.rs
@@ -0,0 +1,117 @@
+use atomic_float::AtomicF32;
+use nih_plug::prelude::{util, Editor, GuiContext};
+use nih_plug_iced::widgets as nih_widgets;
+use nih_plug_iced::*;
+use std::sync::Arc;
+use std::time::Duration;
+use nih_plug_iced::widget::{Column, Space, Text};
+use crate::GainParams;
+
+// Makes sense to also define this here, makes it a bit easier to keep track of
+pub(crate) fn default_state() -> Arc<IcedState> {
+    IcedState::from_size(200, 150)
+}
+
+pub(crate) fn create(
+    params: Arc<GainParams>,
+    peak_meter: Arc<AtomicF32>,
+    editor_state: Arc<IcedState>,
+) -> Option<Box<dyn Editor>> {
+    create_iced_editor::<GainEditor>(editor_state, (params, peak_meter))
+}
+
+struct GainEditor {
+    params: Arc<GainParams>,
+    context: Arc<dyn GuiContext>,
+
+    peak_meter: Arc<AtomicF32>,
+
+    // todo: not sure why this was needed in old version and now isn't
+    // gain_slider_state: nih_widgets::param_slider::State,
+    // peak_meter_state: nih_widgets::peak_meter::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    /// Update a parameter's value.
+    ParamUpdate(nih_widgets::ParamMessage),
+}
+
+impl IcedEditor for GainEditor {
+    type Executor = executor::Default;
+    type Message = Message;
+    type InitializationFlags = (Arc<GainParams>, Arc<AtomicF32>);
+    type Theme = ();
+
+    fn new(
+        (params, peak_meter): Self::InitializationFlags,
+        context: Arc<dyn GuiContext>,
+    ) -> (Self, Task<Self::Message>) {
+        let editor = GainEditor {
+            params,
+            context,
+
+            peak_meter,
+        };
+
+        (editor, Task::none())
+    }
+
+    fn context(&self) -> &dyn GuiContext {
+        self.context.as_ref()
+    }
+
+    fn update(&mut self, message: Self::Message) -> Task<Self::Message> {
+        match message {
+            Message::ParamUpdate(message) => self.handle_param_message(message),
+        }
+
+        Task::none()
+    }
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        Column::new()
+            .align_items(Alignment::Center)
+            .push(
+                Text::new("Gain GUI")
+                    .font(assets::NOTO_SANS_LIGHT)
+                    .size(40)
+                    .height(50.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Bottom),
+            )
+            .push(
+                Text::new("Gain")
+                    .height(20.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .push(
+                nih_widgets::ParamSlider::new(&self.params.gain)
+                    .map(Message::ParamUpdate),
+            )
+            .push(Space::new().height(10))
+            .push(
+                nih_widgets::PeakMeter::new(
+                    util::gain_to_db(self.peak_meter.load(std::sync::atomic::Ordering::Relaxed)),
+                )
+                .hold_time(Duration::from_millis(600)),
+            )
+            .into()
+    }
+
+    fn background_color(&self) -> nih_plug_iced::Color {
+        nih_plug_iced::Color {
+            r: 0.98,
+            g: 0.98,
+            b: 0.98,
+            a: 1.0,
+        }
+    }
+
+    fn theme(&self) -> Self::Theme {
+        todo!()
+    }
+}
